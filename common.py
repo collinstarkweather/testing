0c2892186a5ac68b8b08ed7b300750f16900fe46
Collin Starkweather <github@collinstarkweather.com>
2023-08-23 16:18:57 -0500

    This is a change to the common function

--- common.py
+++ common.py
@@ -1,4 +1,4 @@
 # This script contains common functions
 
 def my_common_function():
-    print("This is a common function")
+    print("This is NOT a common function because it is specific to client 2")
